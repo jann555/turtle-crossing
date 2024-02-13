86545df92b360a812cb76cd1081b6e7212c7ad58
Jannick Ocampo <focampo3@gatech.edu>
2024-02-12 19:29:42 -0500

    create player behaviour

--- player.py
+++ player.py
@@ -1,7 +1,24 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.color("black")
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def move(self):
+        self.fd(MOVE_DISTANCE)
+        if self.ycor() == FINISH_LINE_Y:
+            self. __start_line()
+
+    def __start_line(self):
+        self.clear()
+        self.goto(STARTING_POSITION)
+
