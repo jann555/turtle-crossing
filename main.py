cc56edc3a4156d2c72267e2f26c0ff5533ae8a30
Jannick Ocampo <focampo3@gatech.edu>
2024-02-12 19:30:14 -0500

    Create main behaviour

--- main.py
+++ main.py
@@ -7,8 +7,28 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+
+player = Player()
+car_manager = CarManager()
+
+screen.onkey(player.move, "Up")
 
 game_is_on = True
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.garage:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+
+screen.exitonclick()
+
+
+
